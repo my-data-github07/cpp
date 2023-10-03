b31a8a4f91dbd46e02eeb9171763cf6e0bc8e034
somnat <somnathg.cse@gmail.com>
2023-10-03 22:26:31 +0530

    assignment 6 updated

--- assignment6/Assignment6_1.cpp
+++ assignment6/Assignment6_1.cpp
@@ -1,250 +1,291 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
- //date class
-class date {
-   private:
-   int day;
-   int month;
-   int year;
 
-   public:
-   date()
-   {
-      this->day=0;
-      this->month=0;
-      this->year=0;
-   }
-   date(int day,int month,int year)
-   {
-       this->day=day;
-       this->month=month;
-       this->year=year;
-   }
+// Date class
+class Date
+{
+    // private data members
+private:
+    int day;
+    int month;
+    int year;
 
-   
-   
-   void accept()
-   { 
-      cout<<"Enter the day =";
-      cin>>this->day;
-      cout<<"Enter the month =";
-      cin>>this->month;
-      cout<<"Enter the year =";
-      cin>>this->year;
-
-   }
-
-   void display()
-   {
-    cout<<"*****************************"<<endl;
-    cout<<"Date is ="<<this->day<<"-"<<this->month<<"-"<<this->year<<endl;
-    if(isleepyear())
+    // data functions
+public:
+    // parameter less constructor // default day - -99/-99/1970
+    Date()
     {
-      cout<<"Year is leap year"<<endl;
-    }
-    else
-    {
-      cout<<"Year is not leap year"<<endl;
-    }
-    cout<<"******************************"<<endl;
-   }  
-    bool isleepyear()
-      {
-         if(this->year%400==0)
-         {
-            if(this->year%4==0)
-            {
-               return true;
-            }
-            else
-            {
-               return false;
-            }
-         }
-         else
-         {
-            return false;
-         }
-      }
-
-      void set_day(int day)
-      {
-         this->day=day;
-      }
-      void set_month(int month)
-      {
-         this->month=month;
-      }
-      void set_year(int year)
-      {
-         this->year=year;
-      }
-
-      int get_day()
-      {
-         return this->day;
-      }
-      int get_month()
-      {
-         return this->month;
-      }
-      int get_year()
-      {
-         return this->year;
-      }
-};
-  // employee class
- class employee{
-   private:
-   int id;
-   int sal;
-   string dept;
-   date obj;
-
-
-   public:
-
-   employee()
-   {
-      this->dept="";
-      this->id=0;
-      this->sal=0;
-   }
-   employee(int id,int sal,string dept,int day ,int month,int year):obj(day,month,year)
-    {
-      this->id=id;
-      this->dept=dept;
-      this->sal=sal;
-      
+        this->day = -99;
+        this->month = -99;
+        this->year = 1970;
     }
 
-    void acceptemp()
+    // parameterised constructor
+    Date(int day, int month, int year)
     {
-      cout<<"--------------------"<<endl;
-      cout<<"Enter The id =";
-      cin>>this->id;
-      cout<<"Enter the dept =";
-      cin>>this->dept;
-      cout<<"Enter the sal =";
-      cin>>this->sal;
-      obj.accept();
-      cout<<"--------------------"<<endl;
+        this->day = day;
+        this->month = month;
+        this->year = year;
     }
+
+    // Inspectors
+    int get_day()
+    {
+        return this->day;
+    }
+    int get_month()
+    {
+        return this->month;
+    }
+    int get_year()
+    {
+        return this->year;
+    }
+
+    // Mutators
+
+    void set_day(int day)
+    {
+        this->day = day;
+    }
+
+    void set_month(int month)
+    {
+        this->month = month;
+    }
+
+    void set_year(int year)
+    {
+        this->year = year;
+    }
+
+    // Accept function
+
+    void accept()
+    {
+        cout << "Please Enter day : ";
+        cin >> this->day;
+        cout << "Please Enter month : ";
+        cin >> this->month;
+        cout << "Please Enter year : ";
+        cin >> this->year;
+    }
+
+    // Display function;
+
     void display()
     {
-      cout<<"************************"<<endl;
-      cout<<"id is = "<<this->id<<endl;
-      cout<<"Dept is ="<<this->dept<<endl;
-      cout<<"Sal is ="<<this->sal<<endl;
-      obj.display();
-      cout<<"************************"<<endl;
+
+        cout << "Day : " << this->day << endl;
+        cout << "Month : " << this->month << endl;
+        cout << "Year : " << this->year << endl;
     }
-    int  get_id()
+
+    // Checking for leap year
+
+    bool is_leap_year()
     {
-       return this->id;
+        if ((this->year % 400 == 0) || (this->year % 100 != 0) && (this->year % 4 == 0))
+        {
+            cout << "leap year" << endl;
+            return true;
+        }
+        cout << "Not a leap year" << endl;
+        return false;
     }
-    int get_sal()
+};
+
+// Employee class
+class Employee
+{
+    // private data members
+private:
+    int id;
+    float sal;
+    string dept;
+    Date joining;
+
+    // member functions
+public:
+    // parameterless constructor // default = -99
+    Employee()
     {
-      return this->sal;
+        this->id = -99;
+        this->dept = "-99";
+        this->sal = -99;
+    }
+
+    // parameterized construrtor
+    Employee(int id, float sal, string dept, int day, int month, int year) : joining(day, month, year)
+
+    {
+        this->id = id;
+        this->dept = dept;
+        this->sal = sal;
+    }
+
+    // Inspectors
+    int get_id()
+    {
+        return this->id;
+    }
+    float get_sal()
+    {
+        return this->sal;
     }
     string get_dept()
     {
-      return this->dept;
+        return this->dept;
+    }
+
+    Date get_joining()
+    {
+        return this->joining;
+    }
+
+    // Mutators
+    void set_sal(float sal)
+    {
+        this->sal = sal;
     }
     void set_id(int id)
     {
-      this->id=id;
-    }
-    void set_sal(int sal)
-    {
-       this->sal=sal;
+        this->id = id;
     }
     void set_dept(string dept)
     {
-      this->dept=dept;
+        this->dept = dept;
     }
-    date get_joing_date()
+
+    void set_joining(Date joining)
     {
-       return obj;
+        this->joining = joining;
     }
-    void set_joing_date()
+
+    // Accept
+    void accept()
     {
-      this->obj.accept();
+        cout << "Please Enter id : ";
+        cin >> this->id;
+
+        cout << "Please Enter sal : ";
+        cin >> this->sal;
+
+        cout << "Please Enter dept : ";
+        cin >> this->dept;
+
+        joining.accept();
     }
 
- };
-  //people class
-  class person{
-      private:
-      string name;
-      string addr;
-      date dob;
+    // Display
+    void display()
+    {
+        cout << "Id : " << this->id << endl;
+        cout << "Sal : " << this->sal << endl;
+        cout << "Dept : " << this->dept << endl;
+        joining.display();
+    }
+};
 
-      public:
-      person()
-      {
-         this->name="Mohit";
-         this->addr="karad";
-         
-      }
-      person(string name,string addr ,int day, int month ,int year):dob(day,month,year)
-      {
-         this->name=name;
-         this->addr=addr;
+// Person class
+class Person
+{
+    // private data members
+private:
+    string name;
+    string addr;
+    Date birth_date;
 
-      }
-      void acceptPerson()
-      {
-         cout<<"Enter the name =";
-         cin>>name;
-         cout<<"enter the addres";
-         cin>>addr;
-         dob.accept();
-      }
-      void display()
-      {   cout<<"**********************"<<endl;
-         cout<<"Name is = "<<this->name<<endl;
-         cout<<"Address is = "<<this->addr<<endl;
-         dob.display();
-         cout<<"************************"<<endl;
-         
-      }
+    // member functions
+public:
+    // parameterless constructor // default = -99
+    Person()
+    {
+        this->name = "-99";
+        this->addr = "-99";
+    }
 
-     void set_name(string name)
-     {
-      this->name=name;
-     }
-     void set_addr(string addr)
-     {
-      this->addr=addr;
-     }
-     string get_name()
-     {
-      return this->name;
-     }
-     string get_addr()
-     {
-      return this->addr;
-     }
-     date get_birth_date()
-     {
-       return dob;
-     }
-     void get_birth_date(date dob)
-     {
-      this->dob.accept();
-     }
-  };
+    // parameterized construrtor
+    Person(string name, string addr, int day, int month, int year) : birth_date(day, month, year)
+
+    {
+        this->name = name;
+        this->addr = addr;
+    }
+
+    // Inspectors
+
+    string get_name()
+    {
+        return this->name;
+    }
+
+    string get_addr()
+    {
+        return this->addr;
+    }
+
+    Date get_birth_date()
+    {
+        return this->birth_date;
+    }
+
+    // Mutators
+    void set_name(string name)
+    {
+        this->name = name;
+    }
+
+    void set_addr(string addr)
+    {
+        this->addr = addr;
+    }
+    void set_birth_date(Date birth_date)
+    {
+        this->birth_date = birth_date;
+    }
+
+    // Accept
+    void accept()
+    {
+
+        cout << "Please Enter name : ";
+        cin >> this->name;
+
+        cout << "Please Enter addr : ";
+        cin >> this->addr;
+
+        birth_date.accept();
+    }
+
+    // Display
+    void display()
+    {
+        cout << "name : " << this->name << endl;
+        cout << "addr : " << this->addr << endl;
+        birth_date.display();
+    }
+};
+
 int main()
 {
-   date d;
-   d.accept();
-   d.display();
-   employee e;
-   e.acceptemp();
-   e.display();
-   person p;
-   p.acceptPerson();
-   p.display();
 
+    Employee e1;
+    Employee e2(10, 2000, "dpt1", 20, 20, 2020);
+    Person p1;
+    Person p2("krit", "karad", 30, 30, 3030);
+    e1.display();
+    e2.display();
+    p1.display();
+    p2.display();
+    e1.accept();
+    e2.accept();
+    p1.accept();
+    p2.accept();
+    e1.display();
+    e2.display();
+    p1.display();
+    p2.display();
+
+    return 0;
 }
--- assignment6/Assignment6_2.cpp
+++ assignment6/Assignment6_2.cpp
@@ -1,119 +1,234 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class employee
-{    
-     private:
-     int id;
-     float sal;
-
-     public: 
-     employee()
-     {
-        cout<<"emp constr"<<endl;
-        this->id=0;
-        this->sal=0;
-     }
-     employee(int id,float sal)
-     {
-        this->id=id;
-        this->sal=sal;
-     }
-     void accept()
-     {
-        cout<<"ID = ";
-        cin>>this->id;
-        cout<<"Sal = ";
-        cin>>this->sal;
-     }
-     void display()
-     {
-        cout<<"id is "<<this->id<<endl;
-        cout<<"sal is  "<<this->sal<<endl;
-     }
-
-}; 
-
-class manager : virtual public  employee
+// Employee class
+class Employee
 {
-     private:
-     float bonus;
+    // private data members
+private:
+    int id;
+    float sal;
 
-     public:
-     manager()
-     {
-        cout<<"manager cnst"<<endl;
-        this->bonus=0;
-
-     }
-     manager(float bonus)
-     {
-        this->bonus=bonus;
-     }
-
-     void accept()
-     {
-        cout<<"Enter the bonus";
-        cin>>this->bonus;
-
-     }
-     void display()
-     {
-        cout<<"display bonus"<<this->bonus<<endl;
-    
-     }
-};
-class salesman : virtual public employee
-{    
-    private:
-    float comm;
-
-    public:
-     salesman()
-     {
-        cout<<"slaesman cunst"<<endl;
-       this->comm=0;
-     }
-     salesman(float comm)
-     {
-       this->comm=comm;
-
-     }
-
-     void accept()
-     {
-        cout<<" Enter Commission = ";
-        cin>>this->comm;
-     }
-
-     void display()
-     {
-        cout<<"commission is = "<<this->comm<<endl;
-
-     }
-      
-};
-
-class sales_manager : public salesman ,public manager
-{    public:
-    sales_manager()
+    // member functions
+public:
+    // parameterless constructor // default = -99
+    Employee()
     {
-        cout<<"const for salesmanager"<<endl;
+        this->id = -99;
+
+        this->sal = -99;
     }
-   void accept()
-   {
 
-   }
-     void display()
-     {
-        
-     }
+    // parameterized construrtor
+    Employee(int id, float sal)
 
+    {
+        this->id = id;
+        this->sal = sal;
+    }
+
+    // Inspectors
+    int get_id()
+    {
+        return this->id;
+    }
+    float get_sal()
+    {
+        return this->sal;
+    }
+
+    // Mutators
+    void set_sal(float sal)
+    {
+        this->sal = sal;
+    }
+    void set_id(int id)
+    {
+        this->id = id;
+    }
+
+    // Accept
+    void accept()
+    {
+        cout << "Please Enter id : ";
+        cin >> this->id;
+
+        cout << "Please Enter sal : ";
+        cin >> this->sal;
+    }
+
+    // Display
+    void display()
+    {
+        cout << "Id : " << this->id << endl;
+        cout << "Sal : " << this->sal << endl;
+    }
 };
 
+class Manager : virtual protected Employee
+{
+    // data members
+private:
+    float bonus;
+    // member functions
+public:
+    // Parameterless constructor
+    Manager()
+    {
+        this->bonus = -99;
+    }
+
+    // Parameterised constructor
+    Manager(int id, float sal, float bonus) : Employee::Employee(id, sal)
+    {
+        this->bonus = bonus;
+    }
+
+    // Inspecters
+
+    float get_bonus()
+    {
+        return this->bonus;
+    }
+
+    // Mutators
+    void set_bonus(float bonus)
+    {
+        this->bonus = bonus;
+    }
+
+    // Accept Function
+
+    void accept()
+    {
+        Employee::accept();
+        cout << "Please enter Bonus : ";
+        cin >> this->bonus;
+    }
+
+    // Display Funtion
+
+    void display()
+    {
+        Employee::display();
+        cout << "Bonus :" << this->bonus << endl;
+    }
+
+    // Some protected Functions to maintain Abstraction
+protected:
+    void display_manager()
+    {
+        cout << "Bonus :" << this->bonus << endl;
+    }
+
+    void accept_manager()
+    {
+        cout << "Please enter Bonus : ";
+        cin >> this->bonus;
+    }
+};
+
+// SalesMan class
+class Salesman : virtual protected Employee
+{
+    // data members
+private:
+    float comm;
+    // member functions
+public:
+    // Parameterless constructor
+    Salesman()
+    {
+        this->comm = -99;
+    }
+
+    // Parameterised constructor
+    Salesman(int id, float sal, float comm) : Employee::Employee(id, sal)
+    {
+        this->comm = comm;
+    }
+
+    // Inspecters
+
+    float get_comm()
+    {
+        return this->comm;
+    }
+
+    // Mutators
+    void set_comm(float comm)
+    {
+        this->comm = comm;
+    }
+
+    // Accept Function
+
+    void accept()
+    {
+        Employee::accept();
+        cout << "Please enter comm : ";
+        cin >> this->comm;
+    }
+
+    // Display Funtion
+
+    void display()
+    {
+        Employee::display();
+        cout << "comm :" << this->comm << endl;
+    }
+
+    // Some protected Functions to maintain Abstraction
+protected:
+    void display_salesman()
+    {
+        cout << "comm :" << this->comm << endl;
+    }
+
+    void accept_salesman()
+    {
+        cout << "Please enter comm : ";
+        cin >> this->comm;
+    }
+};
+
+// sales_manager Class
+
+class Sales_manager : Manager, Salesman
+{
+
+public:
+    // Member Functions
+    Sales_manager()
+    {
+    }
+    Sales_manager(int id, float sal, float bonus, float comm) : Employee(id, sal)
+    {
+        Manager::set_bonus(bonus);
+        Salesman::set_comm(comm);
+    }
+
+    // Display function
+    void display()
+    {
+        Employee::display();
+        Manager::display_manager();
+        Salesman::display_salesman();
+    }
+
+    // Accept function
+    void accept()
+    {
+        Employee::accept();
+        Manager::accept_manager();
+        Salesman::display_salesman();
+    }
+};
 
 int main()
 {
-   sales_manager b;
+    Sales_manager sm(1, 2000, 222, 333);
+    sm.display();
 
+    return 0;
 }
